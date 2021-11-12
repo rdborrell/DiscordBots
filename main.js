915b6e9b50c692bb759d12b3c7fa47b8a2594689
Work Git <rborrell@ptc.com>
2021-11-12 14:51:34 -0500

    adding all discord.js files

---
+++ main.js
@@ -0,0 +1,32 @@
+//initialize Discord
+const Discord = require('discord.js');
+const {Client, Intents} = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+//create Bot
+
+const prefix = '!';
+
+client.once('ready', () =>
+{
+    console.log('Vicebot is online.')
+}
+);
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    let greetings = ['Hello', 'Hey', 'Привет', 'Dzień dobry']
+
+    if(command === 'hello' || command === 'hi' || command === 'hey')
+    {
+        message.channel.send(greetings[Math.floor(Math.random()*greetings.length)])
+    }
+});
+
+//log in to bot
+client.login('OTA4NzY2MDQwNDk2OTQzMTI0.YY6gVA.8sFeta4s5WMnloD2eHO6m8HKpxA');
+
+

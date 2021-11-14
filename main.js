8f7209ef183e1c170e9d1c6113dbeffe99027bc3
Work Git <rborrell@ptc.com>
2021-11-14 00:19:07 -0500

    adding other files

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const testSchema = require('./test-schema');
 //initialize Discord
 const Discord = require('discord.js');
 const {Client, Intents} = require('discord.js');
@@ -6,13 +10,22 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 
 const prefix = '!';
 
-client.once('ready', () =>
+client.once('ready', async () =>
 {
     console.log('Vicebot is online.')
-}
-);
+
+    await mongoose.connect(
+        process.env.MONGO_URI,
+        {
+            keepAlive: true,
+        }
+    ).then(()=>{console.log('Connected to database.');
+    }).catch((err) =>{console.log(err);
+})
+});
 
 client.on('message', message =>{
+    
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
@@ -27,6 +40,6 @@ client.on('message', message =>{
 });
 
 //log in to bot
-client.login('OTA4NzY2MDQwNDk2OTQzMTI0.YY6gVA.8sFeta4s5WMnloD2eHO6m8HKpxA');
+client.login('OTA4NzY2MDQwNDk2OTQzMTI0.YY6gVA.raRPEtd-9en1vshMpIWp049lqng');
 
 
